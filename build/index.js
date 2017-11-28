1c528088075db318342a110ab5fa6309e160b371
borisding <boris.ding82@outlook.com>
2017-11-28 16:26:01 +0800

    build entry amendments

---
+++ build/index.js
@@ -0,0 +1,4 @@
+const webpackClient = require('./webpack/client');
+const webpackServer = require('./webpack/server');
+
+module.exports = [webpackClient, webpackServer];
--- build/webpack/index.js
+++
@@ -1,4 +0,0 @@
-const webpackClient = require('./client');
-const webpackServer = require('./server');
-
-module.exports = [webpackClient, webpackServer];
