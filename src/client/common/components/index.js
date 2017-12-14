1a0484d8eadb378b70383cfd5e94c8e64b751f26
borisding <boris.ding82@outlook.com>
2017-12-14 14:46:23 +0800

    added index files. tweaks

---
+++ src/client/common/components/index.js
@@ -0,0 +1,6 @@
+export { default as Checkbox } from './Checkbox';
+export { default as ErrorBoundary } from './ErrorBoundary';
+export { default as HttpStatus } from './HttpStatus';
+export { default as Loader } from './Loader';
+export { default as PageTitle } from './PageTitle';
+export { default as TextInput } from './TextInput';
---
+++ src/client/modules/base/index.js
@@ -0,0 +1,4 @@
+export { default as Body } from './Body';
+export { default as Header } from './Header';
+export { default as Layout } from './Layout';
+export { default as NotFound } from './NotFound';
