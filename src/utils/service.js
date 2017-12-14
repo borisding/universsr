eb408d27c13a66e74bb90fc7e6e9b9b9baac4b0b
borisding <boris.ding82@outlook.com>
2017-12-14 11:37:38 +0800

    tweaks

--- src/utils/service.js
+++ src/utils/service.js
@@ -4,8 +4,18 @@ import { ready } from 'redux-ready-wrapper';
 import { errorCreator, successCreator } from '@redux/middlewares/serviceAlert';
 import { apiBaseUrl, apiVersion } from '@config/index.json';
 
+export const instance = axiosFactory();
+const initial = {};
+const payload = data => data;
+const methods = {
+  GET: 'get',
+  POST: 'post',
+  PUT: 'put',
+  DELETE: 'delete'
+};
+
 // create axios instance with provided config, if any
-export function axiosFactory(userConfig = {}) {
+export function axiosFactory(userConfig = initial) {
   return axios.create(
     extend(
       { baseURL: `${apiBaseUrl}/${apiVersion}`, timeout: 3000 },
@@ -14,30 +24,28 @@ export function axiosFactory(userConfig = {}) {
   );
 }
 
-export const instance = axiosFactory();
-const payload = data => data;
-const methods = {
-  GET: 'get',
-  POST: 'post',
-  PUT: 'put',
-  DELETE: 'delete'
-};
 // make request for action creator based on the provided arguments
 // callback could be, say, a provided normalizer function which returns normalized data
 function makeRequest(
   method,
   url,
-  { type, withReady = true, success = {}, ...config },
+  { type, dispatchReady = true, success = initial, error = initial, ...config },
   callback = payload
 ) {
   const requestor = dispatch =>
     instance
       .request({ method, url, ...config })
-      .then(response => dispatch({ type, payload: callback(response.data) }))
-      .then(action => success.message && dispatch(successCreator(success)))
-      .catch(error => dispatch(errorCreator(error)));
+      .then(res => dispatch({ type, payload: callback(res.data) }))
+      .then(action => {
+        success.message && dispatch(successCreator(success));
+        return action;
+      })
+      .catch(err => {
+        error.message && (err = error);
+        dispatch(errorCreator(err));
+      });
 
-  if (withReady) {
+  if (dispatchReady) {
     return ready(dispatch => requestor(dispatch), {
       isGet: method === methods.GET
     });
