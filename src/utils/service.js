8f576858ee3f5751cf0e6cf470eb8ab103c8b706
borisding <boris.ding82@outlook.com>
2017-12-20 15:14:20 +0800

    axios default interceptors. tweaks

--- src/utils/service.js
+++ src/utils/service.js
@@ -20,6 +20,24 @@ const methods = {
 // create default axios instance
 const instance = axiosFactory();
 
+// axios request/response interceptors for default instance
+// interceptors ref: https://github.com/axios/axios#interceptors
+instance.interceptors.request.use(
+  config => {
+    // we may do something here before request is sent
+    return config;
+  },
+  err => Promise.reject(err)
+);
+
+instance.interceptors.response.use(
+  res => {
+    // we may do something here before returning response data
+    return res;
+  },
+  err => Promise.reject(err)
+);
+
 // create axios instance with provided config, if any
 function axiosFactory(userConfig = initial) {
   return axios.create(
@@ -32,22 +50,20 @@ function axiosFactory(userConfig = initial) {
 
 // make request for action creator based on the provided arguments
 // callback could be, say, a provided normalizer function which returns normalized data
-function makeRequest(
-  method,
-  url,
-  {
+// request config ref: https://github.com/axios/axios#request-config
+function makeRequest(method, url, config, callback = payload) {
+  const {
     type,
     dispatchReady = true,
     error = initial,
     info = initial,
     success = initial,
-    ...config
-  },
-  callback = payload
-) {
+    ...restConfig
+  } = config;
+
   const requestor = dispatch =>
     instance
-      .request({ method, url, ...config })
+      .request({ method, url, ...restConfig })
       .then(res => dispatch({ type, payload: callback(res.data) }))
       .then(action => {
         info.message && dispatch(infoCreator(info));
@@ -71,7 +87,6 @@ function makeRequest(
 
 module.exports = {
   // abstract away respective service requests and dispatchers
-  // request config ref: https://github.com/axios/axios#request-config
   get(url, config, callback) {
     return makeRequest(methods.GET, url, config, callback);
   },
