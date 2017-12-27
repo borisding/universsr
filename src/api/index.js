756b4ac0795b336d9d90812c3db5b49b9f5cd743
borisding <boris.ding82@outlook.com>
2017-12-27 11:24:33 +0800

    middlewares tweaks

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,11 @@
+const isDev = require('isdev');
 const express = require('express');
+const hpp = require('hpp');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('@config');
 const serveApi = require('@bin/serveApi');
+const { logger } = require('@server/middlewares');
 const routers = require('./routers');
 
 const { secret, apiVersion } = config.getProperties();
@@ -10,8 +13,13 @@ const app = express();
 
 // may add more middlewares based on the context
 app
+  .set('etag', !isDev)
+  .set('json spaces', 2)
+  .disable('x-powered-by')
+  .use(logger())
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: true, limit: '10mb' }))
+  .use(hpp()) // after parsed body
   .use(cookieParser(secret))
   .use(`/api/${apiVersion}`, routers);
 
--- src/server/app.js
+++ src/server/app.js
@@ -1,15 +1,12 @@
 const isDev = require('isdev');
 const express = require('express');
-const hpp = require('hpp');
 const helmet = require('helmet');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
 const config = require('@config');
 const syspath = require('@config/syspath');
 const { error, info } = require('@utils');
-const { csp, logger, proxy } = require('./middlewares');
+const { csp, proxy } = require('./middlewares');
 const run = require('./run');
 
 const { secret, publicPath, apiVersion } = config.getProperties();
@@ -17,20 +14,12 @@ const app = express();
 
 app
   .set('etag', !isDev)
-  .set('json spaces', 2)
-  .set('view engine', 'ejs');
-
-app
-  .use(logger())
+  .set('view engine', 'ejs')
   .use(helmet())
   .use(csp.nonce())
   .use(csp.mount(helmet))
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: true, limit: '10mb' }))
-  .use(cors())
   .use(compression())
   .use(cookieParser(secret))
-  .use(hpp()) // after parsed body
   .use(publicPath, express.static(syspath.public))
   .use(`/api/${apiVersion}`, proxy.proxyWeb);
 
