d99e19a1e552c048333c43b6a70cc0b314c43e45
borisding <boris.ding82@outlook.com>
2018-07-17 10:10:50 +0800

    tweaks

--- resources/config/index.js
+++ resources/config/index.js
@@ -1,8 +1,19 @@
 // default config entry file with respective configuration values
 // this entry is going to be used in `src` across app and api
 
+const configProperties = require('./config-properties.json');
+
+// assigned when one of the following is not defined in '.env' file
+if (!configProperties.PORT) {
+  configProperties.PORT = +process.env.PORT || 3000;
+}
+
+if (!configProperties.API_PORT) {
+  configProperties.API_PORT = +process.env.API_PORT || 3030;
+}
+
 // export built config properties for universal usage
-module.exports = require('./config-properties.json');
+module.exports = configProperties;
 
 // export named `SYSPATH` as part of config
 module.exports.SYSPATH = require('./syspath');
