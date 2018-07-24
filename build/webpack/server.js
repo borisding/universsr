3f8a3ef3ba9aa77738c9b3be58ec2ecded033049
borisding <boris.ding82@outlook.com>
2018-07-24 12:01:35 +0800

    using `webpack-node-externals` to exclude for unbundled

--- build/webpack/server.js
+++ build/webpack/server.js
@@ -1,18 +1,10 @@
-const fs = require('fs');
 const isDev = require('isdev');
 const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
 const syspath = require('@config/syspath');
 const webpackCommon = require('./common');
 
 const commonConfig = webpackCommon('server', isDev);
-const externalRegExp = /\.bin|react-universal-component|require-universal-module|webpack-flush-chunks/;
-const nodeExternals = fs
-  .readdirSync(`${syspath.root}/node_modules`)
-  .filter(x => !externalRegExp.test(x))
-  .reduce((externals, mod) => {
-    externals[mod] = `commonjs ${mod}`;
-    return externals;
-  }, {});
 
 module.exports = {
   target: 'node',
@@ -21,7 +13,16 @@ module.exports = {
   context: commonConfig.context,
   devtool: commonConfig.devtool,
   resolve: commonConfig.resolve,
-  externals: nodeExternals,
+  externals: [
+    nodeExternals({
+      whitelist: [
+        'react-universal-component',
+        'require-universal-module',
+        'webpack-flush-chunks',
+        /\.bin/
+      ]
+    })
+  ],
   entry: ['regenerator-runtime/runtime', './app/renderer.js'],
   output: {
     path: `${syspath.src}/app`,
