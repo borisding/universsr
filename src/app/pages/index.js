4200123276c49d5613031878103f827cc29077ab
borisding <boris.ding82@outlook.com>
2018-12-01 17:49:08 +0800

    applied magic comment with `webpackChunkName`

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -1,8 +1,13 @@
 import loadComponent from './loadComponent';
 
 // define and export targeted async components
-export const Home = loadComponent(() => import('./Home'));
-export const Todos = loadComponent(() => import('./Todos'));
+export const Home = loadComponent(() =>
+  import(/* webpackChunkName: 'home' */ './Home')
+);
+
+export const Todos = loadComponent(() =>
+  import(/* webpackChunkName: 'todos' */ './Todos')
+);
 
 // export Root as default, and also NotFound as named
 export { default } from './Root';
