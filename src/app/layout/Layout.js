5ab42303ad34872e34e945ef8540706e800900e2
borisding <boris.ding82@outlook.com>
2018-12-24 11:54:54 +0800

    using named reference for multiple stylesheet imports

--- src/app/layout/Layout.js
+++ src/app/layout/Layout.js
@@ -7,10 +7,11 @@ import { HELMET } from '@config';
 import Header from './Header';
 import Body from './Body';
 import '@common/styles/global.scss';
-import './styles/Layout.scss';
+// eslint-disable-next-line no-unused-vars
+import styles from './styles/Layout.scss';
 
 const Layout = props => (
-  <div styleName="container">
+  <div styleName="styles.container">
     <Helmet {...HELMET} />
     <Header {...props} />
     <Body {...props} />
