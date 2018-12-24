6065639a5ae1b8a76bf74ca8d929c337d1a3717f
borisding <boris.ding82@outlook.com>
2018-12-24 23:40:18 +0800

    ignore styles var pattern for eslint

--- src/app/layout/Layout.js
+++ src/app/layout/Layout.js
@@ -7,7 +7,6 @@ import { HELMET } from '@config';
 import Header from './Header';
 import Body from './Body';
 import '@common/styles/global.scss';
-// eslint-disable-next-line no-unused-vars
 import styles from './styles/Layout.scss';
 
 const Layout = props => (
