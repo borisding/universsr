2d5bb1a3d82de1342f374c1c43cea5a68180fa7a
borisding <boris.ding82@outlook.com>
2018-12-28 11:19:26 +0800

    make promise safe. added client, server output location info

--- resources/scripts/build.js
+++ resources/scripts/build.js
@@ -6,17 +6,22 @@ if (argv === '--analyze') {
   process.env.ANALYZE_MODE = 'enabled';
 }
 
+require('make-promises-safe');
 // clean files before proceeding
 require('./clean');
 // include env script to load targeted environment file
 require('./env');
 
 const colors = require('colors');
+const slash = require('slash');
 const webpack = require('webpack');
 const webpackConfig = require('../webpack/config');
 const SimpleProgressWebpackPlugin = require('simple-progress-webpack-plugin');
 
-function webpackBuild() {
+const clientConfig = webpackConfig[0];
+const serverConfig = webpackConfig[1];
+
+const webpackBuild = () => {
   return new Promise((resolve, reject) => {
     const compiler = webpack(webpackConfig);
     new SimpleProgressWebpackPlugin().apply(compiler);
@@ -29,11 +34,22 @@ function webpackBuild() {
       }
     });
   });
-}
+};
 
 webpackBuild()
   .then(() => {
     console.log(colors.green('Webpack compiled successfully.'));
+    console.log();
+    console.log(
+      colors.cyan(
+        `[CLIENT] Output location: ${slash(clientConfig.output.path)}`
+      )
+    );
+    console.log(
+      colors.cyan(
+        `[SERVER] Output location: ${slash(serverConfig.output.path)}`
+      )
+    );
   })
   .catch(error => {
     console.log(colors.red('ERROR: Webpack failed to compile.'));
