ce1182e3979a97175c3f7d20171de1f5d439fd17
borisding <boris.ding82@outlook.com>
2018-12-28 17:03:27 +0800

    check client/server output before serving.

--- resources/scripts/start.js
+++ resources/scripts/start.js
@@ -8,6 +8,7 @@ const colors = require('colors');
 const slash = require('slash');
 const { spawn } = require('child_process');
 const { SYSPATH } = require('../../config');
+const webpackConfig = require('../webpack/config');
 
 const argv = process.argv.slice(2)[0];
 const argvs = ['--app', '--api'];
@@ -17,14 +18,29 @@ if (!argvs.includes(argv)) {
   process.exit(1);
 }
 
-if (argv === argvs[0] && !fs.existsSync(SYSPATH['PUBLIC'])) {
-  console.log(
-    colors.red(`Action aborted. (${slash(SYSPATH['PUBLIC'])}) does not exist.`)
-  );
-  console.log(
-    colors.yellow('Please run `build` script before starting server.')
-  );
-  process.exit(1);
+// simply check both client and server build output dir/file does exist
+// before running app server for production environment
+if (argv === argvs[0]) {
+  const checkIfOutputExist = output => {
+    if (!fs.existsSync(output)) {
+      console.log(
+        colors.red(`Action aborted. (${slash(output)}) does not exist.`)
+      );
+      console.log(
+        colors.yellow(
+          'Please run `npm run build` script before starting server.'
+        )
+      );
+      process.exit(1);
+    }
+  };
+
+  const clientConfig = webpackConfig[0];
+  const serverConfig = webpackConfig[1];
+  [
+    clientConfig.output.path,
+    `${serverConfig.output.path}/${serverConfig.output.filename}`
+  ].forEach(output => checkIfOutputExist(output));
 }
 
 const runServer = file => {
