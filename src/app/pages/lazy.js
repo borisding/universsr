02ddec6de5296f2acd48fec3dc0a26520064b7b6
borisding <boris.ding82@outlook.com>
2018-12-30 01:12:26 +0800

    allowed to overwrite default options.

--- src/app/pages/lazy.js
+++ src/app/pages/lazy.js
@@ -2,16 +2,22 @@ import universal from 'react-universal-component';
 import { PageLoader } from '@layout';
 
 // page util to lazy load targeted page component by accepting callback
-export default function lazy(page) {
+export default function lazy(page, options = {}) {
   if (typeof page !== 'function') {
     throw new TypeError('Invalid data type of `page`. It must be a function.');
   }
 
-  // for universal api and options,
-  // @see: https://github.com/faceyspacey/react-universal-component#api-and-options
-  return universal(page(), {
+  // default options for async component
+  const defaultOptions = {
     minDelay: parseInt(process.env.MIN_DELAY, 10),
     loadingTransition: false,
     loading: PageLoader
+  };
+
+  // for universal api and options,
+  // @see: https://github.com/faceyspacey/react-universal-component#api-and-options
+  return universal(page(), {
+    ...defaultOptions,
+    ...options
   });
 }
