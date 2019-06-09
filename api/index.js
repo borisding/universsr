a871de6716dcd5a475ca12cb47d90396291e7311
borisding <boris.ding82@outlook.com>
2019-06-09 22:53:59 +0800

    removed default session file storage example.

--- api/index.js
+++ api/index.js
@@ -5,30 +5,15 @@ import express from 'express';
 import hpp from 'hpp';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
-import session from 'express-session';
-import sessionFileStore from 'session-file-store';
-import { syspath } from '@config';
 import { httpLogger, errorHandler, notFoundHandler } from '@middleware';
 import routers from './routers';
 
 const api = express();
 
-// session configuration for file storage
-const FileStore = sessionFileStore(session);
-const fileSession = () =>
-  session({
-    store: new FileStore({ path: `${syspath.storage}/sessions` }),
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.SECRET_KEY,
-    cookie: { maxAge: parseInt(process.env.COOKIE_MAXAGE, 10) }
-  });
-
 api
   .use(httpLogger())
   .use(helmet())
   .use(cookieParser())
-  .use(fileSession())
   .use(express.json())
   .use(express.urlencoded({ extended: true, limit: '10mb' }), hpp())
   .use(`/api/${process.env.API_VERSION}`, routers)
