ae99b57f95d3c933d28b7f4186975bb2f8d12770
borisding <boris.ding82@outlook.com>
2020-02-13 23:38:25 +0800

    move jest mock files to assets

--- jest.config.js
+++ jest.config.js
@@ -11,8 +11,8 @@ module.exports = {
   moduleFileExtensions: ['js', 'jsx'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/config/jest/fileMock.js',
-    '\\.(css|scss)$': '<rootDir>/config/jest/styleMock.js'
+      '<rootDir>/assets/mocks/file.js',
+    '\\.(css|scss)$': '<rootDir>/assets/mocks/style.js'
   },
   testEnvironment: 'node',
   testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.jsx?$',
