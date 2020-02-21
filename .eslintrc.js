1366588d88b8a945339e546bf94cc6a2f55717df
borisding <boris.ding82@outlook.com>
2020-02-21 22:39:16 +0800

    imporove eslint/prettier format consistency

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,16 @@
+const prettierConfig = require('./prettier.config');
+
 module.exports = {
   root: true,
-  parser: "babel-eslint",
+  parser: 'babel-eslint',
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:prettier/recommended",
-    "prettier/react",
-    "prettier"
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'prettier/react',
+    'prettier'
   ],
-  plugins: ["prettier", "jest", "react-hooks"],
+  plugins: ['prettier', 'jest', 'react-hooks'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true
@@ -22,32 +24,25 @@ module.exports = {
   },
   settings: {
     react: {
-      version: "detect"
+      version: 'detect'
     }
   },
   rules: {
-    "global-require": 0,
-    "linebreak-style": 0,
-    "no-global-assign": 0,
-    "no-console": 0,
-    "no-unused-vars": [
+    'global-require': 0,
+    'linebreak-style': 0,
+    'no-global-assign': 0,
+    'no-console': 0,
+    'no-unused-vars': [
       2,
       {
-        varsIgnorePattern: "styles"
+        varsIgnorePattern: 'styles'
       }
     ],
-    "react/prop-types": 0,
-    "react/display-name": 0,
-    "react/jsx-no-target-blank": 1,
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "prettier/prettier": [
-      "error",
-      {
-        singleQuote: true,
-        printWidth: 80,
-        endOfLine: "auto"
-      }
-    ]
+    'react/prop-types': 0,
+    'react/display-name': 0,
+    'react/jsx-no-target-blank': 1,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'prettier/prettier': ['error', prettierConfig]
   }
 };
---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  endOfLine: 'auto'
+};
