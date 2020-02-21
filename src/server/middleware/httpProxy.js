d099818685ed9f4e7513bb10e6b8621ba39990f4
borisding <boris.ding82@outlook.com>
2020-02-21 23:42:00 +0800

    upgrade deps

--- src/server/middleware/httpProxy.js
+++ src/server/middleware/httpProxy.js
@@ -1,8 +1,8 @@
-import proxy from 'http-proxy-middleware';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 // @see: find more on https://github.com/chimurai/http-proxy-middleware
 export default function httpProxy(options = {}) {
-  return proxy({
+  return createProxyMiddleware({
     target: process.env.API_ORIGIN,
     changeOrigin: true,
     ...options
