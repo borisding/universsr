4ca82143722fb37c231324e1a6f7c84fa19c2373
borisding <boris.ding82@outlook.com>
2020-02-21 22:47:21 +0800

    applied stylelint/prettier config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,14 +1,15 @@
+const prettierConfig = require('./prettier.config');
+
 module.exports = {
   plugins: ['stylelint-scss'],
-  extends: 'stylelint-config-sass-guidelines',
+  extends: [
+    'stylelint-config-sass-guidelines',
+    'stylelint-prettier/recommended'
+  ],
   rules: {
     'selector-max-id': 1,
     'selector-class-pattern': null,
-    'property-no-unknown': [
-      true,
-      {
-        ignoreProperties: ['composes']
-      }
-    ]
+    'property-no-unknown': [true, { ignoreProperties: ['composes'] }],
+    'prettier/prettier': [true, prettierConfig]
   }
 };
