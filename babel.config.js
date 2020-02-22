6e006109e8aee1f943bf8b9226578ab260e47d7a
borisding <boris.ding82@outlook.com>
2020-02-22 15:04:49 +0800

    tweaks

--- babel.config.js
+++ babel.config.js
@@ -2,67 +2,63 @@ const { isNode, isTest } = require('./config');
 const { localScopedName } = require('./package');
 const webpackConfig = require('./webpack.config');
 
-function babelConfig() {
-  const presets = [
-    [
-      '@babel/preset-env',
-      {
-        corejs: 3,
-        useBuiltIns: 'usage',
-        targets: !isNode
-          ? { browsers: 'last 2 versions', ie: 11 }
-          : { node: 'current' }
-      }
-    ],
-    '@babel/preset-react'
-  ];
+const presets = [
+  [
+    '@babel/preset-env',
+    {
+      corejs: 3,
+      useBuiltIns: 'usage',
+      targets: !isNode
+        ? { browsers: 'last 2 versions', ie: 11 }
+        : { node: 'current' }
+    }
+  ],
+  '@babel/preset-react'
+];
 
-  const plugins = [
-    'universal-import',
-    '@babel/plugin-transform-strict-mode',
-    '@babel/plugin-proposal-class-properties',
-    [
-      'react-css-modules',
-      {
-        exclude: 'node_modules',
-        context: webpackConfig[0].context, // must match with webpack's context
-        generateScopedName: isTest ? '[local]' : localScopedName,
-        filetypes: {
-          '.scss': {
-            syntax: 'postcss-scss',
-            plugins: ['postcss-nested']
-          }
+const plugins = [
+  'universal-import',
+  '@babel/plugin-transform-strict-mode',
+  '@babel/plugin-proposal-class-properties',
+  [
+    'react-css-modules',
+    {
+      exclude: 'node_modules',
+      context: webpackConfig[0].context, // must match with webpack's context
+      generateScopedName: isTest ? '[local]' : localScopedName,
+      filetypes: {
+        '.scss': {
+          syntax: 'postcss-scss',
+          plugins: ['postcss-nested']
         }
       }
-    ]
-  ];
-
-  const env = {
-    development: {
-      plugins: ['react-hot-loader/babel', ...plugins]
-    },
-    production: {
-      plugins: ['transform-react-remove-prop-types', ...plugins]
-    },
-    test: {
-      plugins: [
-        [
-          'babel-plugin-webpack-alias',
-          {
-            config: webpackConfig,
-            findConfig: true
-          }
-        ],
-        ...plugins
-      ]
     }
-  };
+  ]
+];
 
-  return {
-    presets,
-    plugins,
-    env
-  };
-}
+const env = {
+  development: {
+    plugins: ['react-hot-loader/babel', ...plugins]
+  },
+  production: {
+    plugins: ['transform-react-remove-prop-types', ...plugins]
+  },
+  test: {
+    plugins: [
+      [
+        'babel-plugin-webpack-alias',
+        {
+          config: webpackConfig,
+          findConfig: true
+        }
+      ],
+      ...plugins
+    ]
+  }
+};
 
-module.exports = babelConfig();
+module.exports = {
+  presets,
+  plugins,
+  env
+};
