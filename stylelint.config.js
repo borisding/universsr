7b855ed7363a2ad51f7bd995517db739786caa5f
borisding <boris.ding82@outlook.com>
2020-05-28 22:41:30 +0800

    removed stylelint prettier

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,15 +1,9 @@
-const prettierConfig = require('./prettier.config');
-
 module.exports = {
   plugins: ['stylelint-scss'],
-  extends: [
-    'stylelint-config-sass-guidelines',
-    'stylelint-prettier/recommended'
-  ],
+  extends: ['stylelint-config-sass-guidelines'],
   rules: {
     'selector-max-id': 1,
     'selector-class-pattern': null,
-    'property-no-unknown': [true, { ignoreProperties: ['composes'] }],
-    'prettier/prettier': [true, prettierConfig]
+    'property-no-unknown': [true, { ignoreProperties: ['composes'] }]
   }
 };
