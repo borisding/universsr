064d623d8b790226a7643b42115890c0de3ae14c
borisding <boris.ding82@outlook.com>
2020-08-08 21:57:05 +0800

    add .sass extension

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/assets/mocks/file.js',
-    '\\.(css|scss)$': '<rootDir>/assets/mocks/style.js'
+    '\\.(css|scss|sass)$': '<rootDir>/assets/mocks/style.js'
   },
   testEnvironment: 'node',
   testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.jsx?$',
