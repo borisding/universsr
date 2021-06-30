1db8c08d3c4090069b7967e467039996851a1111
borisding <boris.ding82@outlook.com>
2021-06-30 18:02:34 +0800

    middleware changes

--- app/middleware/errorHandler.js
+++ app/middleware/errorHandler.js
@@ -4,10 +4,10 @@ import { isDev } from '../../config';
 // custom error handler for the app/api
 // only include error stack in development mode
 const errorHandler =
-  ({ json = false } = {}) =>
+  () =>
   // eslint-disable-next-line no-unused-vars
   (err, req, res, next) => {
-    const code = err.statusCode || 500;
+    const code = err.code || 500;
     const stack = isDev ? err.stack : null;
     const error = isDev ? err.message : 'Sorry! Something went wrong.';
     const errorData = { code, error, stack };
@@ -17,9 +17,9 @@ const errorHandler =
     // logs error stack
     logger.error(err.stack);
 
-    // giving erros in JSON format if request made via Ajax or `json` key is true
+    // giving erros in JSON format if request made via Ajax
     // otherwise, rendering 500 template with passed params
-    if (req.xhr || !!json) {
+    if (req.xhr) {
       res.json(errorData);
     } else {
       res.send('500 Internal Server Error.');
