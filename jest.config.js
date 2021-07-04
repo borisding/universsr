50c64864e10507a6c5ba1e61123185f2029bbd4e
borisding <boris.ding82@outlook.com>
2021-07-04 16:33:12 +0800

    edit jest config and mock files

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   setupFiles: ['<rootDir>/env.loader.js'],
-  coverageDirectory: '<rootDir>/assets/coverage',
+  coverageDirectory: '<rootDir>/resources/coverage',
   moduleFileExtensions: ['js', 'jsx'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/assets/mocks/file.js',
-    '\\.(css|scss|sass)$': '<rootDir>/assets/mocks/style.js'
+      '<rootDir>/resources/jest/fileMock.js',
+    '\\.(css|scss|sass)$': '<rootDir>/resources/jest/styleMock.js'
   },
   testEnvironment: 'node',
   testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.jsx?$',
