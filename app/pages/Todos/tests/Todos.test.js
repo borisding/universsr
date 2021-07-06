4b362baaff5ac0bc405226621573b84b3b7c55e9
borisding <boris.ding82@outlook.com>
2021-07-06 12:02:57 +0800

    update test snapshots

--- app/pages/Todos/tests/Todos.test.js
+++ app/pages/Todos/tests/Todos.test.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { MemoryRouter } from 'react-router-dom';
-import { Todos } from '../Todos';
+import Todos from '../Todos';
+
+jest.mock('react-frontload', () => ({
+  useFrontload: () => ({ data: [], frontloadMeta: {} })
+}));
 
 describe('Todos', () => {
   it('renders correctly', () => {
-    const props = {
-      isFetching: false,
-      todos: [{ id: 1, title: 'todo value', completed: false }],
-      actions: {
-        fetchTodos: jest.fn()
-      }
-    };
     const tree = renderer
       .create(
         <MemoryRouter>
-          <Todos {...props} />
+          <Todos />
         </MemoryRouter>
       )
       .toJSON();
--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/resources/jest/fileMock.js',
-    '\\.(css|scss|sass)$': '<rootDir>/resources/jest/styleMock.js'
+    '\\.(css|scss|sass|module)$': '<rootDir>/resources/jest/styleMock.js'
   },
   testEnvironment: 'node',
   testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.jsx?$',
